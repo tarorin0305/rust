2231d4e7df8e401cbb15f38d64593f2b0f671c81
tarorin0305 <dayutakatarorin@gmail.com>
2023-08-14 00:44:30 +0900

    try semantics

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+mod module_hello;
 fn func_ex_div_some(x: i32, y: i32) -> Option<i32> {
-    let ans = if y == 0 {
+    if y == 0 {
         None
     } else {
         Some(x / y)
-    };
-    ans
+    }
 }
 
 fn func_ex_div_result(x: i32, y: i32) -> Result<i32, &'static str> {
@@ -16,7 +16,7 @@ fn func_ex_div_result(x: i32, y: i32) -> Result<i32, &'static str> {
 }
 
 fn func_ex_print_some<T: std::fmt::Display>(ans: Option<T>) {
-    if let Some(x) = ans {
+    if let Some(x) = ans { // ansがSomeの場合、その中の値をxに束縛する
         println!("{}", x);
     } else {
         println!("None");
@@ -37,12 +37,43 @@ fn func_ex_print_result<T: std::fmt::Display, E: std::fmt::Display>(ans: Result<
     }
 }
 
+fn copy_semantics() {
+    let s = 1;
+    let t = s;
+    println!("{}", t);
+    println!("{}", s);
+}
+
+fn move_semantics() {
+    let s = "Hello".to_string();
+    println!("{}", s);
+    let t = s;
+    println!("{}", t);
+    // println!("{}", s); 変数sは "Hello" 文字列の値を格納したメモリ領域への所有権を失っており、他の値の所有権を持っていないため、コンパイルエラーとなる
+}
+
+fn myprint<T: std::fmt::Display>(msg: T) {
+    println!("{}", msg);
+}
+
+fn call_functions() {
+    let s = "Hello".to_string();
+    let ss = s.clone();
+    myprint(s);
+    myprint(ss);
+}
+
 fn main() {
     // call all functions
-    func_ex_print_some(func_ex_div_some(10, 5));
-    func_ex_print_some(func_ex_div_some(10, 0));
-    func_ex_print_some_match(func_ex_div_some(10, 5));
-    func_ex_print_some_match(func_ex_div_some(10, 0));
-    func_ex_print_result(func_ex_div_result(10, 5));
-    func_ex_print_result(func_ex_div_result(10, 0));
+    // func_ex_print_some(func_ex_div_some(10, 5));
+    // func_ex_print_some(func_ex_div_some(10, 0));
+    // func_ex_print_some_match(func_ex_div_some(10, 5));
+    // func_ex_print_some_match(func_ex_div_some(10, 0));
+    // func_ex_print_result(func_ex_div_result(10, 5));
+    // func_ex_print_result(func_ex_div_result(10, 0));
+    // call module function
+    module_hello::print_hello();
+    copy_semantics();
+    move_semantics();
+    call_functions();
 }
